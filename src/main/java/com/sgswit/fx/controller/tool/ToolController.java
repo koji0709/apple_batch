6de3c8b48e929ec151e6abaf9906f7a6eeb21458
阳刚 <806688405@qq.com>
2023-11-10 17:14:42 +0800

    工具箱

---
+++ src/main/java/com/sgswit/fx/controller/tool/ToolController.java
@@ -0,0 +1,258 @@
+package com.sgswit.fx.controller.tool;
+
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.swing.clipboard.ClipboardUtil;
+import cn.hutool.core.util.StrUtil;
+import com.sgswit.fx.utils.StringUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.DragEvent;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author yanggang
+ * @createTime 2023/10/31
+ */
+public class ToolController extends StrUtil {
+
+
+    @FXML
+    private TextArea sourceText;
+    @FXML
+    private TextArea compareText;
+    @FXML
+    private RadioButton box;
+    @FXML
+    private Label segmentation;
+    @FXML
+    private Label repeat;
+    @FXML
+    private TextField textField;
+    @FXML
+    private Label current;
+    @FXML
+    private Label current1;
+
+    //文本去重
+    @FXML
+    public void distinct() {
+
+        List<String> source = List.of(sourceText.getText().split("\n"));
+        ArrayList arrayList = new ArrayList(source);
+        if (StringUtils.isEmpty(source.get(0))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("请输入要去除的源文本！");
+
+            alert.showAndWait();
+            return;
+        }
+        List<String> compare = List.of(compareText.getText().split("\n"));
+        ArrayList arrayList1 = new ArrayList(compare);
+        if (StringUtils.isEmpty(compare.get(0))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("请输入要去除的文本！");
+
+            alert.showAndWait();
+            return;
+        }
+        Integer num = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = 0; j < arrayList1.size(); j++) {
+                if (arrayList.get(i).equals(arrayList1.get(j))) {
+                    arrayList.remove(i);
+                    arrayList1.remove(j);
+                    num++;
+                }
+            }
+        }
+        String join = String.join("\n", arrayList);
+        sourceText.setText(join);
+        compareText.setText("");
+        String s = "";
+        if (box.isSelected()) {
+            s = "已复制到剪辑板";
+            ClipboardUtil.setStr(join);
+        } else {
+            s = "否（未勾选）";
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("操作详细");
+        alert.setHeaderText("");
+
+        alert.setContentText("\n源文本: " + source.size() +
+                "\n\n\n重复数: " + num +
+                "\n\n\n剩余数: " + arrayList.size() +
+                "\n\n\n是否复制到剪切板: " + s +
+                "\n\n\n剩余数据在源文本编辑框中，可在右边继续添加要去除的文本!");
+
+
+        alert.showAndWait();
+
+    }
+
+
+    //超大文件分割
+    @FXML
+    public void splitDataToSaveFile() {
+        Integer rows = Integer.valueOf(textField.getText());
+        String str = segmentation.getText();
+        File sourceFile = new File(str);
+        if (!"txt".equals(FileUtil.getSuffix(sourceFile))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("请拖拽需要分割的txt文件到指定区域后再执行此操作！");
+
+            alert.showAndWait();
+            return;
+        }
+        String targetDirectoryPath = sourceFile.getParent();
+        long startTime = System.currentTimeMillis();
+        List<File> fileList = new ArrayList<>();
+        File targetFile = new File(targetDirectoryPath);
+        if (!sourceFile.exists() || rows <= 0 || sourceFile.isDirectory()) {
+            return;
+        }
+        if (targetFile.exists()) {
+            if (!targetFile.isDirectory()) {
+                return;
+            }
+        } else {
+            targetFile.mkdirs();
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream(sourceFile);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String lineStr;
+            int lineNo = 1, fileNum = 1;
+            while ((lineStr = bufferedReader.readLine()) != null) {
+                stringBuilder.append(lineStr).append("\r\n");
+                if (lineNo % rows == 0) {
+                    File file = new File(targetDirectoryPath + File.separator + "分割文本" + "-" + fileNum + "-" + sourceFile.getName());
+                    writeFile(stringBuilder.toString(), file);
+                    //清空文本
+                    stringBuilder.delete(0, stringBuilder.length());
+                    fileNum++;
+                    fileList.add(file);
+                }
+                lineNo++;
+            }
+            if ((lineNo - 1) % rows != 0) {
+                File file = new File(targetDirectoryPath + File.separator + "分割文本" + "-" + fileNum + "-" + sourceFile.getName());
+                writeFile(stringBuilder.toString(), file);
+                fileList.add(file);
+            }
+            current.setText("分割完成，共计分割" + fileNum + "份，文件保存在软件同目录的【文件处理】文件夹中！");
+            long endTime = System.currentTimeMillis();
+
+        } catch (Exception e) {
+            throw new SecurityException("分割文件异常", e);
+        }
+
+    }
+
+    private void writeFile(String text, File file) {
+        try (
+                FileOutputStream fileOutputStream = new FileOutputStream(file);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter, 1024)
+        ) {
+            bufferedWriter.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //文本文件行去重
+    @FXML
+    public void current() {
+        String filePath = repeat.getText();
+        Set<String> lines = new HashSet<>();
+        File file = new File(filePath);
+        if (!"txt".equals(FileUtil.getSuffix(file))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("请拖拽需要分割的txt文件到指定区域后再执行此操作！");
+
+            alert.showAndWait();
+            return;
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line.trim()); // 去除行首尾的空白字符
+            }
+
+
+            FileWriter fileWriter = new FileWriter(file.getParent() + "/去重文件" + file.getName());
+            for (String string : lines) {
+                if (!string.trim().equals("")) {
+                    fileWriter.write(string.trim() + "\r\n");
+                }
+            }
+            current1.setText("去重复完成，文件保存在软件同目录的【文件处理】文件夹中！");
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @FXML
+    void getFile(DragEvent event) {
+        List<File> files = event.getDragboard().getFiles();
+        //获得文件
+        File file = files.get(0);
+        //之后的相关操作，获得文件路径等..
+        if (!"txt".equals(FileUtil.getSuffix(file))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("本工具仅支持txt文件！");
+
+            alert.showAndWait();
+        }
+        //之后的相关操作，获得文件路径等..
+        repeat.setText(file.getPath());
+    }
+
+    @FXML
+    void getFiles(DragEvent event) {
+        List<File> files = event.getDragboard().getFiles();
+        //获得文件
+        File file = files.get(0);
+        if (!"txt".equals(FileUtil.getSuffix(file))) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("信息: ");
+            alert.setHeaderText("");
+            alert.setContentText("本工具仅支持txt文件！");
+
+            alert.showAndWait();
+        }
+        //之后的相关操作，获得文件路径等..
+        segmentation.setText(file.getPath());
+    }
+
+}
--- src/main/java/com/sgswit/fx/controller/tool/ToolboxltemsController.java
+++ src/main/java/com/sgswit/fx/controller/tool/ToolboxltemsController.java
@@ -19,14 +19,17 @@ import java.io.IOException;
  * @createTime 2023/09/27
  */
 public class ToolboxltemsController {
-
-
-    public void onPersonalCenterClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/query/security-question-query.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/tool/toolbox.fxml"));
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 1100, 700);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
         Stage popupStage = new Stage();
-        popupStage.setTitle("个人中心");
+        popupStage.setTitle("工具箱");
         popupStage.initModality(Modality.WINDOW_MODAL);
         popupStage.setScene(scene);
         popupStage.setResizable(false);
@@ -34,42 +37,5 @@ public class ToolboxltemsController {
         popupStage.show();
     }
 
-    public void onwbbjqcBtnClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/query/security-question-query.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
-        Stage popupStage = new Stage();
-        popupStage.setTitle("文本比较去重");
-        popupStage.initModality(Modality.WINDOW_MODAL);
-        popupStage.setScene(scene);
-        popupStage.setResizable(false);
-        popupStage.initStyle(StageStyle.UTILITY);
-        popupStage.show();
-    }
 
-    public void oncdwjfgBtnClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/query/security-question-query.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
-        Stage popupStage = new Stage();
-        popupStage.setTitle("超大文件分割");
-        popupStage.initModality(Modality.WINDOW_MODAL);
-        popupStage.setScene(scene);
-        popupStage.setResizable(false);
-        popupStage.initStyle(StageStyle.UTILITY);
-        popupStage.show();
-    }
-
-    public void onqccfwbBtnClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/query/security-question-query.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 700);
-        scene.getRoot().setStyle("-fx-font-family: 'serif'");
-        Stage popupStage = new Stage();
-        popupStage.setTitle("去除重复文本");
-        popupStage.initModality(Modality.WINDOW_MODAL);
-        popupStage.setScene(scene);
-        popupStage.setResizable(false);
-        popupStage.initStyle(StageStyle.UTILITY);
-        popupStage.show();
-    }
 }
