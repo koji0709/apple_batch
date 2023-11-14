e0783c7f18abc96f754a7b5362c80f32b21ba88f
dengjunling <1215489895@qq.com>
2023-11-14 23:28:42 +0800

    修改弹出风格

--- src/main/java/com/sgswit/fx/enums/StageEnum.java
+++ src/main/java/com/sgswit/fx/enums/StageEnum.java
@@ -9,30 +9,30 @@ public enum StageEnum {
     MAIN("欢迎使用APPLE批量处理程序","views/main-view.fxml",400,520,StageStyle.UNDECORATED,Modality.NONE,""),
 
     /********** OPERATION区 ***********/
-    ACCOUNT_INFO_MODIFY("官方修改资料","views/operation/account-info-modify.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    UNLOCK_CHANGE_PASSWORD("账号解锁改密","views/operation/unlock-change-password.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    UPDATE_APPLE_ID("苹果更改账号","views/operation/update-appleid.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    SECURITY_UPGRADE("开通双重认证","views/operation/security-upgrade.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    SECURITY_DOWNGRADE("关闭双重认证","views/operation/security-downgrade.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    SUPPORT_PIN("生成支持PIN","views/operation/support-pin.fxml",1260,760,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
+    ACCOUNT_INFO_MODIFY("官方修改资料","views/operation/account-info-modify.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    UNLOCK_CHANGE_PASSWORD("账号解锁改密","views/operation/unlock-change-password.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    UPDATE_APPLE_ID("苹果更改账号","views/operation/update-appleid.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    SECURITY_UPGRADE("开通双重认证","views/operation/security-upgrade.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    SECURITY_DOWNGRADE("关闭双重认证","views/operation/security-downgrade.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    SUPPORT_PIN("生成支持PIN","views/operation/support-pin.fxml",1260,760,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
 
     /********** ICLOUD区 ***********/
-    CHECK_WHETHER("能否登录iCloud","views/iCloud/check-whether-icloud.fxml",1000,650,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
+    CHECK_WHETHER("能否登录iCloud","views/iCloud/check-whether-icloud.fxml",1000,650,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
 
     /********** ITUNES区 ***********/
-    COUNTRY_MODIFY("账号国家修改","views/iTunes/country-modify.fxml",1100,650,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    DELETE_PAYMENT("删除付款方式","views/iTunes/payment-method.fxml",1100,650,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    GIFTCARD_BLANCE("礼品卡查余额","views/iTunes/giftCard-balance-check.fxml",1100,650,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
+    COUNTRY_MODIFY("账号国家修改","views/iTunes/country-modify.fxml",1100,650,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    DELETE_PAYMENT("删除付款方式","views/iTunes/payment-method.fxml",1100,650,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    GIFTCARD_BLANCE("礼品卡查余额","views/iTunes/giftCard-balance-check.fxml",1100,650,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
 
 
     /********** QUERY区 ***********/
-    BIRTHDAY_COUNTRY_QUERY("查询生日国家","views/query/birthday-country.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    BALANCE_QUERY("密保查询余额","views/query/balance-query.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    CHECK_GRAY_BALANCE("检测灰余额","views/query/detection-gray-balance.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    WHETHER_APPLEID("检测是否AppleID","views/query/whether-appleid.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    RAPID_FILTRATION("急速过滤密正","views/query/rapid-filtration.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    DETECTION_WHETHER("检测是否过检","views/query/detection-whether-inspection.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
-    SECURITY_QUESTION("查询密保问题","views/query/security-question-query.fxml",1100,700,StageStyle.DECORATED,Modality.APPLICATION_MODAL,""),
+    BIRTHDAY_COUNTRY_QUERY("查询生日国家","views/query/birthday-country.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    BALANCE_QUERY("密保查询余额","views/query/balance-query.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    CHECK_GRAY_BALANCE("检测灰余额","views/query/detection-gray-balance.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    WHETHER_APPLEID("检测是否AppleID","views/query/whether-appleid.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    RAPID_FILTRATION("急速过滤密正","views/query/rapid-filtration.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    DETECTION_WHETHER("检测是否过检","views/query/detection-whether-inspection.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
+    SECURITY_QUESTION("查询密保问题","views/query/security-question-query.fxml",1100,700,StageStyle.DECORATED,Modality.WINDOW_MODAL,""),
 
     /********** TOOLBOX区 ***********/
 
