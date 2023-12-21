90aa0324e1413267d8317de98ebdc73dc272ffcf
dengjunling <1215489895@qq.com>
2023-12-21 18:50:05 +0800

    绑定虚拟卡

---
+++ src/main/java/com/sgswit/fx/controller/common/CommSecuritycodePopupController.java
@@ -0,0 +1,58 @@
+package com.sgswit.fx.controller.common;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author DELL
+ */
+public class CommSecuritycodePopupController {
+    @FXML
+    private TextField smsTextField;
+    @FXML
+    private Label sCodeLabel;
+
+    private String securityCode = "";
+    private String account      = "";
+
+    public CommSecuritycodePopupController(){
+
+    }
+
+
+    @FXML
+    private void onConfirmBtnClick(ActionEvent actionEvent) throws Exception{
+        securityCode=smsTextField.getText();
+        if(StringUtils.isEmpty(securityCode)){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("验证码不能为空");
+            alert.show();
+            return;
+        }
+        Button button= (Button) actionEvent.getSource();
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
+    }
+
+    public String getSecurityCode(){
+        return this.securityCode;
+    }
+
+    public void setAccount(String account){
+        this.account = account;
+        this.sCodeLabel.setText("请输入账户[ "+ account +" ]的双重验证码");
+    }
+
+    public void onCancelBtnClick(ActionEvent actionEvent) {
+        Button cancelBtn= (Button) actionEvent.getSource();
+        Stage stage = (Stage) cancelBtn.getScene().getWindow();
+        stage.close();
+    }
+}
