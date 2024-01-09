027355e0c277b69a3fd89f3209b1b4bc8802b642
dengjunling <1215489895@qq.com>
2024-01-09 22:11:15 +0800

    删除付款方式，添加双重认证

--- src/main/java/com/sgswit/fx/controller/iTunes/PaymentMethodController.java
+++ src/main/java/com/sgswit/fx/controller/iTunes/PaymentMethodController.java
@@ -24,15 +24,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.paint.Paint;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 /**
@@ -101,66 +101,7 @@ public class PaymentMethodController extends CustomTableView<Account> implements
             if (!StrUtil.isEmptyIfStr(account.getNote())) {
                 continue;
             }else{
-                accountQueryBtn.setText("正在查询");
-                accountQueryBtn.setTextFill(Paint.valueOf("#FF0000"));
-                accountQueryBtn.setDisable(true);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run(){
-                        try {
-                            try {
-                                account.setHasFinished(false);
-                                account.setNote("正在登录...");
-                                accountTableView.refresh();
-                                Map<String,Object> res= PurchaseBillUtil.iTunesAuth(account.getAccount(),account.getPwd());
-                                if(!res.get("code").equals(Constant.SUCCESS)){
-                                    account.setDataStatus("0");
-                                    account.setNote(res.get("msg").toString());
-                                    tableRefreshAndInsertLocal(account, res.get("msg").toString());
-                                }else{
-                                    boolean hasInspectionFlag= (boolean) res.get("hasInspectionFlag");
-                                    if(!hasInspectionFlag){
-                                        account.setDataStatus("0");
-                                        tableRefreshAndInsertLocal(account,"此 Apple ID 尚未用于 App Store。");
-                                        return;
-                                    }
-                                    account.setNote("登录成功，数据删除中...");
-                                    accountTableView.refresh();
-                                    res=ITunesUtil.delPaymentInfos(res);
-                                    if(res.get("code").equals(Constant.SUCCESS)){
-                                        account.setDataStatus("1");
-                                        tableRefreshAndInsertLocal(account, "删除成功");
-                                    }else{
-                                        account.setDataStatus("0");
-                                        tableRefreshAndInsertLocal(account, MapUtils.getStr(res,"msg"));
-                                    }
-
-                                }
-                                account.setHasFinished(true);
-                                accountTableView.refresh();
-                            } catch (Exception e) {
-                                account.setDataStatus("0");
-                                account.setHasFinished(true);
-                                tableRefreshAndInsertLocal(account, "操作失败，接口异常");
-                                accountQueryBtn.setDisable(false);
-                                accountQueryBtn.setText("开始执行");
-                                accountQueryBtn.setTextFill(Paint.valueOf("#238142"));
-                            }
-                        }finally {
-                            //JavaFX Application Thread会逐个阻塞的执行这些任务
-                            Platform.runLater(new Task<Integer>() {
-                                @Override
-                                protected Integer call() {
-                                    setAccountNumLabel();
-                                    accountQueryBtn.setDisable(false);
-                                    accountQueryBtn.setText("开始执行");
-                                    accountQueryBtn.setTextFill(Paint.valueOf("#238142"));
-                                    return 1;
-                                }
-                            });
-                        }
-                    }
-                }).start();
+                executeFun(account);
             }
 
         }
@@ -176,5 +117,106 @@ public class PaymentMethodController extends CustomTableView<Account> implements
         super.insertLocalHistory(List.of(account));
     }
     public void onStopBtnClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onContentMenuClick(ContextMenuEvent contextMenuEvent) {
+        List<String> items=new ArrayList<>(super.menuItem) ;
+        items.add(Constant.RightContextMenu.TWO_FACTOR_CODE.getCode());
+        super.onContentMenuClick(contextMenuEvent,accountTableView,items);
+    }
+    protected  void executeFun(Account account){
+        accountQueryBtn.setText("正在查询");
+        accountQueryBtn.setTextFill(Paint.valueOf("#FF0000"));
+        accountQueryBtn.setDisable(true);
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                try {
+                    try {
+                        account.setHasFinished(false);
+                        account.setNote("正在登录...");
+                        accountTableView.refresh();
+                        String step= StringUtils.isEmpty(account.getStep())?"01":account.getStep();
+                        Map<String,Object> res=account.getAuthData();
+                        if("00".equals(step)){
+                            String authCode=account.getAuthCode();
+                            res= PurchaseBillUtil.iTunesAuth(authCode,res);
+                        }else if("01".equals(step)){
+                            res= PurchaseBillUtil.iTunesAuth(account.getAccount(),account.getPwd());
+                        }else{
+                            res=new HashMap<>();
+                        }
+                        if(Constant.TWO_FACTOR_AUTHENTICATION.equals(res.get("code"))) {
+                            account.setNote(String.valueOf(res.get("msg")));
+                            account.setAuthData(res);
+                        }else if(!res.get("code").equals(Constant.SUCCESS)){
+                            account.setDataStatus("0");
+                            account.setNote(res.get("msg").toString());
+                            tableRefreshAndInsertLocal(account, res.get("msg").toString());
+                        }else{
+                            boolean hasInspectionFlag= (boolean) res.get("hasInspectionFlag");
+                            if(!hasInspectionFlag){
+                                account.setHasFinished(true);
+                                account.setDataStatus("0");
+                                tableRefreshAndInsertLocal(account,"此 Apple ID 尚未用于 App Store。");
+                                return;
+                            }
+                            account.setNote("登录成功，数据删除中...");
+                            accountTableView.refresh();
+                            res=ITunesUtil.delPaymentInfos(res);
+                            if(res.get("code").equals(Constant.SUCCESS)){
+                                account.setDataStatus("1");
+                                tableRefreshAndInsertLocal(account, MapUtils.getStr(res,"msg"));
+                            }else{
+                                account.setDataStatus("0");
+                                tableRefreshAndInsertLocal(account, MapUtils.getStr(res,"msg"));
+                            }
+
+                        }
+                        account.setHasFinished(true);
+                        accountTableView.refresh();
+                    } catch (Exception e) {
+                        account.setDataStatus("0");
+                        account.setHasFinished(true);
+                        tableRefreshAndInsertLocal(account, "操作失败，接口异常");
+                        accountQueryBtn.setDisable(false);
+                        accountQueryBtn.setText("开始执行");
+                        accountQueryBtn.setTextFill(Paint.valueOf("#238142"));
+                    }
+                }finally {
+                    //JavaFX Application Thread会逐个阻塞的执行这些任务
+                    Platform.runLater(new Task<Integer>() {
+                        @Override
+                        protected Integer call() {
+                            setAccountNumLabel();
+                            accountQueryBtn.setDisable(false);
+                            accountQueryBtn.setText("开始执行");
+                            accountQueryBtn.setTextFill(Paint.valueOf("#238142"));
+                            return 1;
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+    /**重新执行**/
+    @Override
+    protected void reExecute(Account account){
+        executeFun(account);
+    }
+    @Override
+    protected void twoFactorCodeExecute(Account account, String authCode){
+        try{
+            Map<String,Object> res=account.getAuthData();
+            if(Constant.TWO_FACTOR_AUTHENTICATION.equals(MapUtils.getStr(res,"code"))){
+                account.setAuthCode(authCode);
+                account.setStep("00");
+                executeFun(account);
+            }else{
+                alert("未下发双重验证码");
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
