9086def50a9a03e8086c7630c2d9beec93fe30bc
dengjunling <1215489895@qq.com>
2024-01-30 22:25:15 +0800

    主页面-综合设置

--- src/main/java/com/sgswit/fx/controller/base/IntegratedSettingsController.java
+++ src/main/java/com/sgswit/fx/controller/base/IntegratedSettingsController.java
@@ -1,10 +1,18 @@
 package com.sgswit.fx.controller.base;
 
 import com.sgswit.fx.enums.ProxyEnum;
+import com.sgswit.fx.utils.PropertiesUtil;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
 import java.util.Map;
@@ -20,10 +28,29 @@ import java.util.ResourceBundle;
 public class IntegratedSettingsController implements Initializable {
     @FXML
     public ComboBox<Map<String,String>> proxyType;
+    @FXML
+    public TextField proxyApiUrl;
+    @FXML
+    public CheckBox proxyApiNeedPass;
+    @FXML
+    public TextField proxyApiUser;
+    @FXML
+    public TextField proxyApiPass;
+    @FXML
+    public TextField proxyTunnelAddress;
+    @FXML
+    public TextField proxyTunnelUser;
+    @FXML
+    public TextField proxyTunnelPass;
+    @FXML
+    public TextField sendTimeOut;
+    @FXML
+    public GridPane rootPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         proxyTypeFn();
+        initParas();
     }
     /**快捷国家资料下拉**/
     protected void proxyTypeFn(){
@@ -41,5 +68,43 @@ public class IntegratedSettingsController implements Initializable {
                 return null;
             }
         });
+    }
+    protected void initParas(){
+        proxyApiUrl.setText(PropertiesUtil.getOtherConfig("proxyApiUrl"));
+        proxyApiUser.setText(PropertiesUtil.getOtherConfig("proxyApiUser"));
+        proxyApiPass.setText(PropertiesUtil.getOtherConfig("proxyApiPass"));
+        proxyTunnelAddress.setText(PropertiesUtil.getOtherConfig("proxyTunnelAddress"));
+        proxyTunnelUser.setText(PropertiesUtil.getOtherConfig("proxyTunnelUser"));
+        proxyTunnelPass.setText(PropertiesUtil.getOtherConfig("proxyTunnelPass"));
+        sendTimeOut.setText(PropertiesUtil.getOtherConfig("sendTimeOut"));
+        if(StringUtils.isEmpty(PropertiesUtil.getOtherConfig("proxyApiNeedPass")) || !Boolean.valueOf(PropertiesUtil.getOtherConfig("proxyApiNeedPass"))){
+            proxyApiNeedPass.setSelected(false);
+        }else{
+            proxyApiNeedPass.setSelected(true);
+        }
+        String proxyType= PropertiesUtil.getOtherConfig("proxyType");
+        if(!StringUtils.isEmpty(proxyType)){
+            this.proxyType.getSelectionModel().select(Integer.valueOf(proxyType)-1);
+        }
+    }
+    @FXML
+    public void proxyTunnelAddressCheckAction(ActionEvent actionEvent) {
+
+    }
+    @FXML
+    public void saveAction(ActionEvent actionEvent) throws Exception {
+        PropertiesUtil.setOtherConfig("proxyApiUrl", proxyApiUrl.getText());
+        PropertiesUtil.setOtherConfig("proxyApiUser", proxyApiUser.getText());
+        PropertiesUtil.setOtherConfig("proxyApiPass", proxyApiPass.getText());
+        PropertiesUtil.setOtherConfig("proxyTunnelAddress", proxyTunnelAddress.getText());
+        PropertiesUtil.setOtherConfig("proxyTunnelUser", proxyTunnelUser.getText());
+        PropertiesUtil.setOtherConfig("proxyTunnelPass", proxyTunnelPass.getText());
+        PropertiesUtil.setOtherConfig("sendTimeOut", sendTimeOut.getText());
+        PropertiesUtil.setOtherConfig("proxyApiNeedPass", proxyApiNeedPass.isSelected()?"true":"");
+        int index=proxyType.getSelectionModel().getSelectedIndex()+1;
+        PropertiesUtil.setOtherConfig("proxyType", index>0?String.valueOf(index):"");
+        Button button= (Button) actionEvent.getSource();
+        Stage stage = (Stage) button.getScene().getWindow();
+        stage.close();
     }
 }
